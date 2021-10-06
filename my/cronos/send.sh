14ba1f477df8e1df9d74b0d6ba75ac4e067c280a
jongwhan <jonghwan@crypto.com>
2021-10-07 00:20:37 +0900

    OK

--- my/cronos/send.sh
+++ my/cronos/send.sh
@@ -3,6 +3,7 @@ export FROM=$S1
 export TO=$S2
 
 
-export AMOUNT=2100000000000000001$DENOM
+#export AMOUNT=2100000000000000001$DENOM
+export AMOUNT=1$DENOM
 echo "send amount $AMOUNT"
 $CLI tx bank  send $FROM $TO $AMOUNT --chain-id $CHAINID --keyring-backend $KEYRING  --fees 20$DENOM --home $CHAINHOME
