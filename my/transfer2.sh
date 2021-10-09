af75428b7a9afaf1fd80f28826c2a43f4c5866fa
jongwhan <jonghwan@crypto.com>
2021-10-10 01:25:35 +0900

    OK

--- my/transfer2.sh
+++ my/transfer2.sh
@@ -1,2 +1,3 @@
 . ./setup.sh
-hermes -c $MYCONFIG tx raw ft-transfer $MYIBC0 $MYIBC1 transfer $MYCHANNEL 1 -o 1000 -n 1 -d basetcro  -r  cro1u08u5dvtnpmlpdq333uj9tcj75yceggszxpnsy   -k testkey
+#hermes -c $MYCONFIG tx raw ft-transfer $MYIBC0 $MYIBC1 transfer $MYCHANNEL 10000000000 -o 1000 -n 1 -d basetcro  -r  cro1u08u5dvtnpmlpdq333uj9tcj75yceggszxpnsy   -k testkey
+hermes -c $MYCONFIG tx raw ft-transfer $MYIBC0 $MYIBC1 transfer $MYCHANNEL 10000000000 -o 1000 -n 1 -d basetcro  -r cro1apdh4yc2lnpephevc6lmpvkyv6s5cjh652n6e4  -k testkey
