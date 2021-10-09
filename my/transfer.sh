9e741bffa8028713c20656845249fb70509dafae
jongwhan <jonghwan@crypto.com>
2021-10-09 18:20:58 +0900

    OK

--- my/transfer.sh
+++ my/transfer.sh
@@ -6,4 +6,5 @@
 #    amount                    amount of coins (samoleans, by default) to send (e.g. `100000`)
                   
 echo "send" $MYIBC0 "->" $MYIBC1
-hermes -c $MYCONFIG tx raw ft-transfer $MYIBC1 $MYIBC0 transfer $MYCHANNEL 1 -o 1000 -n 1 -d basecro -r crc16z0herz998946wr659lr84c8c556da55dc34hh -k testkey2
+#hermes -c $MYCONFIG tx raw ft-transfer $MYIBC1 $MYIBC0 transfer $MYCHANNEL 1 -o 1000 -n 1 -d basecro -r crc16z0herz998946wr659lr84c8c556da55dc34hh -k testkey2
+hermes -c $MYCONFIG tx raw ft-transfer $MYIBC1 $MYIBC0 transfer $MYCHANNEL 1 -o 1000 -n 1 -d basecro -r crc1q04jewhxw4xxu3vlg3rc85240h9q7ns6hglz0g   -k testkey2
